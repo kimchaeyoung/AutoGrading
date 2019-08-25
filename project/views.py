631cb98a304a48342739d33d4dd3b03bfb1e5e00
HaMinJi <h.river771@gmail.com>
2019-08-25 21:18:45 +0900

    create / push

--- project/views.py
+++ project/views.py
@@ -3,34 +3,37 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 import subprocess
+import pprint
+import os
 
 @csrf_exempt
 def git_alert(request):
-    result = 'ping'
+    result = 'pingping'
     if 'payload' in request.POST:
         payload = json.loads(request.POST['payload'])
-        git_url = payload['repository']['clone_url']        
-        command = 'git clone ' + str(git_url) 
+        pprint.pprint(payload)
+        print(payload['created'])
+        if payload['created'] is True:
+            result = "created"
+            print("o")
+            #student_name = payload['commits']['author']['name']
+            clone_url = payload['repository']['clone_url']
+            #subprocess.call(['mkdir', student_name])
+            #subprocess.call(['cd', student_name])
+            
+            command = 'git clone ' + str(clone_url) 
+            command = command.split()
+            subprocess.Popen(command, stdin=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines = True)
 
-#        subprocess.call(["git_file"], shell=True)
-#        command = "git pull"
-
-        command = command.split()
-        subprocess.Popen(command, stdin=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines = True)
-
-    result = subprocess.check_output(["python", "git_file/hello.py", "3"])  # test case
-
-    result = int(result.decode('utf-8'))
-
-    if result is 9:  # auto grading
-        result = subprocess.check_output(["python", "git_file/hello.py", "8"])  # test case
-        result = int(result.decode('utf-8'))
-        if result is 64:
-            result = "pass"
         else:
-            result = "fail"
-    else :
-        result = "fail"
+            result = "push"
+            print("x")
+            cwd = os.getcwd()
+            os.chdir(cwd + '/test6')
+            command = 'git pull'
+            command = command.split()
+            subprocess.call(command)
+            os.chdir(cwd)
 
 
     return render(request, "push.html", {"result": result})
