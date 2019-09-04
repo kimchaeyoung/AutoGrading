95422da2ac0059329bf334093cb4cf3a03451a68
jihyegnb <jihyegnb@naver.com>
2019-09-04 11:17:27 +0900

    second

--- Django/project/models.py
+++ Django/project/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
+
 from django.db import models
 
-# Create your models here.
+class Classrooms(models.Model):
+    organization = models.CharField(max_length=200)
+
+class Homeworks(models.Model):
+    classroom = models.ForeignKey(Classrooms, on_delete=models.CASCADE)
+    hw_name = models.CharField(max_length=200)
+
+
+class Classroom_student(models.Model):
+    classroom = models.CharField(max_length=200)
+    student = models.CharField(max_length=200)
+
+class Homework_student(models.Model):
+    homework = models.CharField(max_length=200)
+    student = models.CharField(max_length=200)
+
+class Professor(models.Model):
+    professor_id = models.CharField(max_length=50)
+    professor_name = models.CharField(max_length=50)
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=50)
+    student_name = models.CharField(max_length=50)
--- Django/project/views.py
+++ Django/project/views.py
@@ -7,6 +7,8 @@ import pprint
 import os
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
+from .models import *
+
 
 def home(request):
     return render(request, 'home.html')
@@ -18,7 +20,21 @@ def student(request):
     return render(request, 'student.html')
 
 def professor(request):
+    if request.user != None:
+        professor = request.user.username
+        if not Professor.objects.filter(professor_id=professor).exists():
+            p=Professor(professor_id=professor, professor_name=professor)
+            p.save()
+            print(professor)
     return render(request, 'professor.html')
+
+def check_task(request):
+    return render(request, 'check_task.html')
+
+def mypage(request):
+    current_user=request.user
+    data = Student.objects.get(name=current_user.username)
+    return render(request, 'mypage.html', {'student': data})
 
 class UsersView(TemplateView):
     template_name='studentlist.html'
@@ -32,15 +48,81 @@ def classroom(request):
     return render(request, 'class.html')   
 
 @csrf_exempt
+def createhw(request):
+    if 'payload' in request.POST:
+        payload = json.loads(request.POST['payload'])
+        action = payload['action']
+        hwname = payload['repository']['name']
+        organization = payload['repository']['owner']['login']
+       
+        if '-' in hwname: #학생이 hw을 accept해서 rp가 create되었을 때
+            words = hwname.split("-")
+
+            if action=="created":
+                if not Classroom_student.objects.filter(classroom=organization).filter(student=words[1]).exists():
+                    cs = Classroom_student(classroom=organization, student=words[1])
+                    cs.save()
+                    if not Homework_student.objects.filter(homework=words[0].upper()).filter(student=words[1]).exists():
+                        hs = Homework_student(homework=words[0].upper(), student=words[1])
+                        hs.save()
+
+            elif action=="deleted":
+                if Homework_student.objects.filter(homework=words[0].upper()).filter(student=words[1]).exists():
+                    hs = Homework_student(homework=words[0].upper(), student=words[1])
+                    hs.delete()
+
+ 
+        else: #교수님이 문제를 내서 rp가 create 되었을때 / 교수님께 HW name에 절대 - 를 포함해서는 안된다고 안내해야함 - 별로다...
+
+ 
+            if action=="created":
+                if Classrooms.objects.filter(organization=organization).exists():
+                    c = Classrooms.objects.get(organization=organization)
+                else:
+                    c = Classrooms(organization=organization)
+                    c.save()
+                h = Homeworks(classroom=c, hw_name=hwname)
+                h.save()
+
+            elif action=="deleted":
+                if Classrooms.objects.filter(organization=organization).exists():
+                    if Homeworks.objects.filter(hw_name=hwname).exists():
+                        h = Homeworks.objects.get(hw_name=hwname)
+                        h.delete()
+
+    hws = Homeworks.objects.all()
+    return render(request, "hwlist.html", {'hws':hws})
+            
+
+def myhw(request):
+        my_hws = Homework_student.objects.get(student="joyful96")
+        return render(request, "myhw.html", {'hws':my_hws})
+
+
+'''
+@csrf_exempt
 def git_alert(request):
     result = 'pingping'
     if 'payload' in request.POST:
         payload = json.loads(request.POST['payload'])
+         
+        if payload['action'] is not null:
+            if payload['action']=="created":
+                assignment=payload['repository']['name']
+                print(assignment)
+
+
+        if payload['pusher']['name']!=null:
+            student=payload['pusher']['name']
+            if student != "jihyegnb":
+                s=Student(name=student, status="commit")
+                s.save()
+
 
         if payload['created'] is True:
             result = "created"
             clone_url = payload['repository']['clone_url']
-            
+           
             command = 'git clone ' + str(clone_url) 
             command = command.split()
             subprocess.Popen(command, stdin=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines = True)
@@ -68,23 +150,13 @@ def test_code(request):
    
    print(result)
 
-
-   ''' 
-    if result is 9:
-        result = subprocess.check_output(["python", "git_files/hello.py", "8"])
-        result = int(result.decode('utf-8'))
-        if result is 64:
-            result = "pass"
-        else:
-            result = "fail"
-   '''
    if result is 9 :
         result = "pass"
    else:
         result = "fail"
 
    return JsonResponse(result, safe=False)
-
+'''
 
      
 
